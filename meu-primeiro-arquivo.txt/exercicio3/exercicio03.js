fab5f403e71bd0e6161546e2d5c0c3ec2a427e41
LUCAS MANHAES BALBINO <lucas.mbalbino@senacsp.edu.br>
2022-09-16 22:18:47 -0300

    atualizando branch

---
+++ meu-primeiro-arquivo.txt/exercicio3/exercicio03.js
@@ -0,0 +1,16 @@
+let element;
+
+// element = document.doctype;
+
+// element = document.forms;
+// element = document.forms[0];
+// element = document.forms[0][1];
+// element = document.forms[0][1];
+//  element = document.script[2].getAttribute('src');
+ element = document.querySelector('div#task');
+
+ console.log(element);
+
+
+
+
